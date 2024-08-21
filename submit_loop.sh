0618764b5fb96b29a61486dee919b98ec784f9cb
Sean Bresnahan <seantbresnahan3@gmail.com>
2024-08-21 12:28:03 -0500

    Update submit_loop.sh
    Add command line argument $2 to handle specification of log directory. Modify bsub call to include -o and -e flags pointing to directory assigned with $2.

--- submit_loop.sh
+++ submit_loop.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 sample_IDs="${1}"
+DIR_LOGS="${2}"
 
 FILES=( $(awk '{print $1}' ./${sample_IDs}) )
 
 for FILE in "${FILES[@]}"
 do
-  bsub -env XFILEX=${FILE} < run_processPEfastq.lsf
+  bsub -env XFILEX=${FILE} \
+    -o ${DIR_LOGS}/${FILE}.logo \
+    -e ${DIR_LOGS}/${FILE}.loge \
+    < run_processPEfastq.lsf
 done
